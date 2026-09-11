9a135ea826b8a9d4ea1996f0b159f955969e9529
pieromg67 <mgpiero3@gmail.com>
2026-09-10 22:53:46 -0500

    Update MainActivity.kt

--- app/src/main/java/com/example/moviecounter/MainActivity.kt
+++ app/src/main/java/com/example/moviecounter/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.moviecounter
-
+// Editado por Companero A en remoto
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
